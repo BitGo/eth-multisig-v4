12495b758480d94ca3fbb3a96864636e8bbe89dd
Mark Toda <mtoda@bitgo.com>
2020-08-12 16:50:59 -0700

    Bump solcjs version

--- mocha-test/compile.js
+++ mocha-test/compile.js
@@ -8,38 +8,63 @@ const solc = require('solc');
 
 // Useful when async func goes wrong
 process.on('unhandledRejection', (r) => {
-  console.log('UnhandledRejection');
-  console.error(r);
+    console.log('UnhandledRejection');
+    console.error(r);
 });
 
 describe('Contracts', async () => {
-  const contracts = [
-    'ERC20Interface.sol',
-    'FixedSupplyToken.sol',
-    'Forwarder.sol',
-    'WalletSimple.sol',
-    'WalletSimple.sol',
-    'coins/EtcWalletSimple.sol',
-    'coins/RskWalletSimple.sol',
-    'coins/CeloWalletSimple.sol',
-  ];
+    const contracts = [
+        'ERC20Interface.sol',
+        'FixedSupplyToken.sol',
+        'Forwarder.sol',
+        'WalletSimple.sol',
+        'coins/EtcWalletSimple.sol',
+        'coins/RskWalletSimple.sol',
+        'coins/CeloWalletSimple.sol',
+        'WalletFactory.sol',
+        'ForwarderFactory.sol',
+        'CloneFactory.sol',
+    ];
 
-  let result;
+    let result;
 
-  before(async function () {
-    // solc takes a while
-    this.timeout(10000);
-    const contents = await Promise.all(contracts.map(async (filename) =>
-      (await promisify(fs.readFile)('./contracts/' + filename)).toString()
-    ));
-    const sources = _.zipObject(contracts, contents);
-    result = solc.compile({ sources }, 1);
-  });
+    before(async function () {
+        // solc takes a while
+        this.timeout(10000);
+        const contents = await Promise.all(contracts.map(async (filename) =>
+            (await promisify(fs.readFile)('./contracts/' + filename)).toString()
+        ));
 
-  it('compile without warnings and errors', () => {
-    should.equal(
-      (result.errors || []).length, 0,
-      (result.errors || []).join('\n')
-    );
-  });
+        sources = {};
+        for (let i = 0; i < contracts.length; i++) {
+            const contract = contracts[i];
+            const content = contents[i];
+            sources[contract] = content;
+        }
+
+        const input = {
+            language: 'Solidity',
+            sources: {
+                'test.sol': {
+                    content: 'contract C { function f() public { } }'
+                }
+            },
+            settings: {
+                outputSelection: {
+                    '*': {
+                        '*': ['*']
+                    }
+                }
+            }
+        };
+
+        result = solc.compile(JSON.stringify(input));
+    });
+
+    it('compile without warnings and errors', () => {
+        should.equal(
+            (result.errors || []).length, 0,
+            (result.errors || []).join('\n')
+        );
+    });
 });
