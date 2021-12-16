ce0c78e7d328054b67d58e1da74c2232c9256693
Ryan Lee <ryanlee@bitgo.com>
2021-12-16 12:36:01 -0500

    fix(forwarder): separate erc721 and erc1155 flush params
    ticket: BG-40686

--- contracts/ForwarderFactory.sol
+++ contracts/ForwarderFactory.sol
@@ -9,7 +9,8 @@ contract ForwarderFactory is CloneFactory {
   event ForwarderCreated(
     address newForwarderAddress,
     address parentAddress,
-    bool shouldAutoFlushERC721
+    bool shouldAutoFlushERC721,
+    bool shouldAutoFlushERC1155
   );
 
   constructor(address _implementationAddress) {
@@ -17,19 +18,20 @@ contract ForwarderFactory is CloneFactory {
   }
 
   function createForwarder(address parent, bytes32 salt) external {
-    this.createForwarder(parent, salt, true);
+    this.createForwarder(parent, salt, true, true);
   }
 
   function createForwarder(
     address parent,
     bytes32 salt,
-    bool shouldAutoFlushERC721
+    bool shouldAutoFlushERC721,
+    bool shouldAutoFlushERC1155
   ) external {
     // include the signers in the salt so any contract deployed to a given address must have the same signers
     bytes32 finalSalt = keccak256(abi.encodePacked(parent, salt));
 
     address payable clone = createClone(implementationAddress, finalSalt);
-    Forwarder(clone).init(parent, shouldAutoFlushERC721);
-    emit ForwarderCreated(clone, parent, shouldAutoFlushERC721);
+    Forwarder(clone).init(parent, shouldAutoFlushERC721, shouldAutoFlushERC1155);
+    emit ForwarderCreated(clone, parent, shouldAutoFlushERC721, shouldAutoFlushERC1155);
   }
 }
