8a87511d0b21e56a24256d804ccecc42441b8686
Ryan Lee <ryanlee@bitgo.com>
2022-01-05 11:29:58 -0500

    chore: add truffle deployment scripts
    ticket: BG-41092

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,16 @@
-module.exports = function (deployer) {};
+const WalletSimple = artifacts.require('WalletSimple');
+const WalletFactory = artifacts.require('WalletFactory');
+const Forwarder = artifacts.require('Forwarder');
+const ForwarderFactory = artifacts.require('ForwarderFactory');
+
+module.exports = async function (deployer) {
+  await deployer.deploy(WalletSimple);
+  const walletSimple = await WalletSimple.deployed();
+
+  await deployer.deploy(WalletFactory, walletSimple.address);
+
+  await deployer.deploy(Forwarder);
+  const forwarder = await Forwarder.deployed();
+
+  await deployer.deploy(ForwarderFactory, forwarder.address);
+};
--- truffle.js
+++ truffle.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { API_URL, MNEMONIC } = process.env;
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id
+      gas: 5800000,
+      websockets: true
+    },
+    live: {
+      provider: function () {
+        return new HDWalletProvider(MNEMONIC, API_URL);
+      },
+      network_id: '*',
       gas: 5800000,
       websockets: true
     }
