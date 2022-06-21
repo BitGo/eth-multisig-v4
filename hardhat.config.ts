89fd4d89cf3cb393f5da66d38704449aa24551f2
Mithun Gudipati <mithungudipati@bitgo.com>
2022-06-21 05:19:13 -0700

    chore: modify hardhat script to accept polygon api key
    Ticket: BG-46442

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,13 @@ import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 
-const { PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } = process.env;
+const {
+  PRIVATE_KEY,
+  ALCHEMY_API_KEY,
+  ETHERSCAN_API_KEY,
+  POLYGON_API_KEY,
+  POLYGONSCAN_API_KEY
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.10',
@@ -42,11 +48,12 @@ const config: HardhatUserConfig = {
       accounts: [`${PRIVATE_KEY}`]
     },
     matic: {
-      url: `https://polygon-mainnet.g.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://polygon-mainnet.g.alchemyapi.io/v2/${POLYGON_API_KEY}`,
       accounts: [`${PRIVATE_KEY}`]
     },
     tmatic: {
-      url: `https://polygon-mumbai.g.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      //https://polygon-mumbai.g.alchemy.com/
+      url: `https://polygon-mumbai.g.alchemyapi.io/v2/${POLYGON_API_KEY}`,
       accounts: [`${PRIVATE_KEY}`]
     }
   },
@@ -56,7 +63,14 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     // Same API KEY for all env's
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: {
+      //ethereum
+      mainnet: `${ETHERSCAN_API_KEY}`,
+      goerli: `${ETHERSCAN_API_KEY}`,
+      //polygon
+      polygon: `${POLYGONSCAN_API_KEY}`,
+      polygonMumbai: `${POLYGONSCAN_API_KEY}`
+    }
   },
   mocha: {
     timeout: 100000
