812f80cd7724b67899ef3ba999864391bd122412
Akash Gianchandani <akashgianchandani@bitgo.com>
2023-12-18 18:21:27 +0530

    fix(eth-multisig-v4): add chain id of the coin in the network identifier WIN-1423

--- contracts/coins/OpethWalletSimple.sol
+++ contracts/coins/OpethWalletSimple.sol
@@ -25,8 +25,8 @@ import '../WalletSimple.sol';
  * Unlike eth_sign, the message is not prefixed.
  *
  * The operationHash the result of keccak256(prefix, toAddress, value, data, expireTime).
- * For ether transactions, `prefix` is "OPETH".
- * For token transaction, `prefix` is "OPETH-ERC20" and `data` is the tokenContractAddress.
+ * For ether transactions, `prefix` is chain id of the coin i.e. for optimism mainnet it is "10"
+ * For token transaction, `prefix` is "10-ERC20" and `data` is the tokenContractAddress.
  *
  *
  */
@@ -35,23 +35,23 @@ contract OpethWalletSimple is WalletSimple {
    * Get the network identifier that signers must sign over
    * This provides protection signatures being replayed on other chains
    */
-  function getNetworkId() internal override pure returns (string memory) {
-    return 'OPETH';
+  function getNetworkId() internal override view returns (string memory) {
+    return Strings.toString(block.chainid);
   }
 
   /**
    * Get the network identifier that signers must sign over for token transfers
    * This provides protection signatures being replayed on other chains
    */
-  function getTokenNetworkId() internal override pure returns (string memory) {
-    return 'OPETH-ERC20';
+  function getTokenNetworkId() internal override view returns (string memory) {
+    return string.concat(Strings.toString(block.chainid), '-ERC20');
   }
 
   /**
    * Get the network identifier that signers must sign over for batch transfers
    * This provides protection signatures being replayed on other chains
    */
-  function getBatchNetworkId() internal override pure returns (string memory) {
-    return 'OPETH-Batch';
+  function getBatchNetworkId() internal override view returns (string memory) {
+    return string.concat(Strings.toString(block.chainid), '-Batch');
   }
 }
