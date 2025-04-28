13ae67cb82a24d4d895d0f359fafd68ed76361f8
yogeshwar-bitgo <yogeshwarbisht136@bitgo.com>
2025-04-28 14:21:00 +0530

    chore(eth-multisig-v4): add somnia config for contract deployment     Ticket: WIN-5015

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,7 +40,8 @@ const {
   XDC_EXPLORER_API_KEY,
   WEMIX_EXPLORER_API_KEY,
   BERA_RPC_URL,
-  MONAD_EXPLORER_API_KEY
+  MONAD_EXPLORER_API_KEY,
+  SOMNIA_EXPLORER_API_KEY
 } = process.env;
 
 const PLACEHOLDER_KEY: string =
@@ -201,6 +202,22 @@ const config: HardhatUserConfig = {
         `${PRIVATE_KEY_FOR_BATCHER_CONTRACT_DEPLOYMENT}`
       ]
     },
+    tstt: {
+      url: `https://dream-rpc.somnia.network/`,
+      accounts: [
+        `${PRIVATE_KEY_FOR_V4_CONTRACT_DEPLOYMENT}`,
+        `${PLACEHOLDER_KEY}`,
+        `${PRIVATE_KEY_FOR_BATCHER_CONTRACT_DEPLOYMENT}`
+      ]
+    },
+    stt: {
+      url: `https://dream-rpc.somnia.network/`, //TODO: WIN-5278: change it with mainnet explorer, when its available
+      accounts: [
+        `${PRIVATE_KEY_FOR_V4_CONTRACT_DEPLOYMENT}`,
+        `${PLACEHOLDER_KEY}`,
+        `${PRIVATE_KEY_FOR_BATCHER_CONTRACT_DEPLOYMENT}`
+      ]
+    },
     tmon: {
       url: `https://testnet-rpc.monad.xyz/`,
       accounts: [
@@ -343,7 +360,10 @@ const config: HardhatUserConfig = {
       wemixMainnet: `${WEMIX_EXPLORER_API_KEY}`,
       //Monad
       monadTestnet: `${MONAD_EXPLORER_API_KEY}`,
-      monadMainnet: `${MONAD_EXPLORER_API_KEY}`
+      monadMainnet: `${MONAD_EXPLORER_API_KEY}`,
+      //Somnia
+      somniaTestnet: `${SOMNIA_EXPLORER_API_KEY}`,
+      somniaMainnet: `${SOMNIA_EXPLORER_API_KEY}`
     },
     customChains: [
       {
@@ -476,6 +496,22 @@ const config: HardhatUserConfig = {
           apiURL:
             'https://api.routescan.io/v2/network/testnet/evm/43113/etherscan/api',
           browserURL: 'https://testnet.snowtrace.io/'
+        }
+      },
+      {
+        network: 'somniaTestnet',
+        chainId: 50312,
+        urls: {
+          apiURL: 'https://shannon-explorer.somnia.network/api',
+          browserURL: 'https://shannon-explorer.somnia.network/'
+        }
+      },
+      {
+        network: 'somniaMainnet',
+        chainId: 50312, //TODO: WIN-5278: change it with mainnet explorer, when its available
+        urls: {
+          apiURL: 'https://shannon-explorer.somnia.network/api', //TODO: WIN-5278: change it with mainnet explorer, when its available
+          browserURL: 'https://shannon-explorer.somnia.network/' //TODO: WIN-5278: change it with mainnet explorer, when its available
         }
       },
       {
